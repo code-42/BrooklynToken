f047d4ed001f127ac477999d9884906140bdc989
Edward Dupre <edwd42@gmail.com>
2018-11-15 22:19:27 -0500

    add function to transfer tokens

--- BrooklynToken.sol
+++ BrooklynToken.sol
@@ -15,7 +15,7 @@ contract BrooklynToken {
     // Create an array with all balances
     mapping (address => uint256) public balanceOf;
     
-
+    // function that will deploy our smart contract
     constructor (uint256 initialSupply, string _tokenName, string _tokenSymbol) public {
 
         // Give the initial supply to the contract owner
@@ -26,6 +26,26 @@ contract BrooklynToken {
 
         // Set the token symbol
         tokenSymbol = _tokenSymbol;
+
+    }
+
+    // enable ability to transfer tokens
+    function transfer(address _to, uint256 _value) public returns (bool success) {
+
+        // Check if the sender has enough
+        require(balanceOf[msg.sender] >= _value);
+
+        // Check for integer overflows
+        require(balanceOf[_to] + _value >= balanceOf[_to]);
+
+        // Subtract value from the sender
+        balanceOf[msg.sender] -= _value;
+
+        // Add value to recipient
+        balanceOf[_to] += _value;
+
+        // Return true if transfer is successful
+        return true;
 
     }
 
