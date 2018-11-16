d4459ee92b67ea466b9aeb037cea9b4c2d4c8bff
Edward Dupre <edwd42@gmail.com>
2018-11-15 22:15:47 -0500

    initialize variables

--- BrooklynToken.sol
+++ BrooklynToken.sol
@@ -3,4 +3,16 @@ pragma solidity ^0.4.20;
 
 contract BrooklynToken {
 
+// Set the contract owner
+    address public owner = msg.sender;
+
+    // Initialize tokenName
+    string public tokenName;
+
+    // Initialize tokenSymbol
+    string public tokenSymbol;
+
+    // Create an array with all balances
+    mapping (address => uint256) public balanceOf;
+    
 }
