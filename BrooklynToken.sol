a9081e64e26bf8643e97ab0105f95086878bbd3f
Edward Dupre <edwd42@gmail.com>
2018-11-15 22:17:30 -0500

    add constructor

--- BrooklynToken.sol
+++ BrooklynToken.sol
@@ -15,4 +15,19 @@ contract BrooklynToken {
     // Create an array with all balances
     mapping (address => uint256) public balanceOf;
     
+
+    constructor (uint256 initialSupply, string _tokenName, string _tokenSymbol) public {
+
+        // Give the initial supply to the contract owner
+        balanceOf[owner] = initialSupply;
+
+        // Set the token name
+        tokenName = _tokenName;
+
+        // Set the token symbol
+        tokenSymbol = _tokenSymbol;
+
+    }
+
 }
+
